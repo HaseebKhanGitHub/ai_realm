e52a353b9bede4cf491dfe5f702bc2c6036a6574
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-12-14 00:27:19 +0500

    commit

---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,69 @@
+import 'package:ai_realm/screens/homeScreen.dart';
+import 'package:flutter/material.dart';
+
+
+import '../helper/global.dart';
+import '../widgets/custom_loading.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    //wait for some time on splash & then move to next screen
+    /*Future.delayed(const Duration(seconds: 2), () {
+      Get.off(() =>
+      Pref.showOnboarding ? const OnboardingScreen() : const homeScreen());
+    });*/
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //initializing device size
+    mq = MediaQuery.sizeOf(context);
+
+    return Scaffold(
+      //body
+      body: SizedBox(
+        width: double.maxFinite,
+        child: Column(
+          children: [
+            //for adding some space
+            const Spacer(flex: 2),
+
+            //logo
+            Card(
+              color: Colors.indigo.shade400,
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(20))),
+              child: Padding(
+                padding: EdgeInsets.all(mq.width * .05),
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  width: mq.width * .4,
+                ),
+              ),
+            ),
+
+            //for adding some space
+            const Spacer(),
+
+            //lottie loading
+            const CustomLoading(),
+
+            //for adding some space
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
