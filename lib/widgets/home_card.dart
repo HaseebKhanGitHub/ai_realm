e9765633f99c461b3f198a161ed93fa2752e98d9
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-12-18 00:34:55 +0500

    commit

---
+++ lib/widgets/home_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class home_card extends StatelessWidget {
+  const home_card({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card();
+  }
+}
