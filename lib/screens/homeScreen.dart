9398e9d51d2f0f825f21817467ebce99b5ef4288
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-12-18 00:34:59 +0500

    commit

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,5 +1,7 @@
 
 
+import 'package:ai_realm/helper/global.dart';
+import 'package:ai_realm/widgets/home_card.dart';
 import 'package:flutter/material.dart';
 
 class homeScreen extends StatefulWidget {
@@ -12,6 +14,20 @@ class homeScreen extends StatefulWidget {
 class _homeScreenState extends State<homeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text(appName,style: TextStyle(color: Colors.blue,fontSize:20,fontWeight: FontWeight.bold),),
+      backgroundColor: Colors.white,centerTitle: true,elevation: 2,
+      actions: [
+IconButton(onPressed: (){}, icon: Icon(Icons.brightness_4_outlined,color: Colors.blue,size: 26,))
+      ],
+
+      ),
+
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+            horizontal: mq.width * .04, vertical: mq.height * .015),
+
+        children: [home_card()],),
+    );
   }
 }
