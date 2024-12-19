1d4f94ae7d5b8fb78db6f8a7c1fc14cc941e58e0
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-12-20 00:57:04 +0500

    commit

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,9 +1,8 @@
-
-
 import 'package:ai_realm/helper/global.dart';
 import 'package:ai_realm/model/home_type.dart';
 import 'package:ai_realm/widgets/home_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class homeScreen extends StatefulWidget {
   const homeScreen({super.key});
@@ -16,19 +15,34 @@ class _homeScreenState extends State<homeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(appName,style: TextStyle(color: Colors.blue,fontSize:20,fontWeight: FontWeight.bold),),
-      backgroundColor: Colors.white,centerTitle: true,elevation: 2,
-      actions: [
-IconButton(onPressed: (){}, icon: Icon(Icons.brightness_4_outlined,color: Colors.blue,size: 26,))
-      ],
-
+      appBar: AppBar(
+        title: Text(
+          appName,
+          style: TextStyle(
+              color: Colors.blue, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 2,
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.brightness_4_outlined,
+                color: Colors.blue,
+                size: 26,
+              ))
+        ],
       ),
-
       body: ListView(
         padding: EdgeInsets.symmetric(
             horizontal: mq.width * .04, vertical: mq.height * .015),
-
-        children: homeType.values.map((e)=>home_card(hometype: e,)).toList(),),
+        children: homeType.values
+            .map((e) => home_card(
+                  hometype: e,
+                ))
+            .toList(),
+      ),
     );
   }
 }
