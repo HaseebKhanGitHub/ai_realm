0510d88a4368cdae1699dda7b97f119633dde447
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-12-20 00:56:50 +0500

    commit

--- lib/widgets/home_card.dart
+++ lib/widgets/home_card.dart
@@ -2,6 +2,7 @@ import 'package:ai_realm/model/home_type.dart';
 import 'package:flutter/material.dart';
 
 import 'package:ai_realm/helper/global.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:lottie/lottie.dart';
 
 class home_card extends StatelessWidget {
@@ -13,34 +14,60 @@ class home_card extends StatelessWidget {
   Widget build(BuildContext context) {
     /* Animate.restartOnHotReload = true;*/
 
-    return Container(
-      width: double.infinity,
+    return InkWell(
+      onTap: hometype.onTap,
       child: Card(
         color: Colors.blue.withOpacity(.2),
         elevation: 0,
         margin: EdgeInsets.only(bottom: mq.height * .02),
         shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.all(Radius.circular(20))),
-        child: Row(
-          children: [
-            Lottie.asset(
-              "assets/lottie/${hometype.lottie}",
-              width: mq.width * .35,
-            ),
-            Spacer(),
-            Text(
-              hometype.title,
-              style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  letterSpacing: 1),
-            ),
-            Spacer(
-              flex: 2,
-            )
-          ],
-        ),
-      ),
+        child: hometype.leftAlign
+            ? Row(
+                children: [
+                  Padding(
+                    padding: hometype.padding,
+                    child: Lottie.asset(
+                      "assets/lottie/${hometype.lottie}",
+                      width: mq.width * .35,
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    hometype.title,
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 1),
+                  ),
+                  Spacer(
+                    flex: 2,
+                  )
+                ],
+              )
+            : Row(
+                children: [
+                  Spacer(flex: 2),
+                  Text(
+                    hometype.title,
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 1),
+                  ),
+                  Spacer(),
+                  Container(
+                    width: mq.width * .35,
+                    child: Padding(
+                      padding: hometype.padding,
+                      child: Lottie.asset(
+                        "assets/lottie/${hometype.lottie}",
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+      ).animate().fade(duration: 1.seconds),
     );
   }
 }
