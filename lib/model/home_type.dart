68550b140651b791d96d5d17bef8e98012ba8f85
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-12-20 00:56:56 +0500

    commit

--- lib/model/home_type.dart
+++ lib/model/home_type.dart
@@ -1,3 +1,10 @@
+import 'package:ai_realm/screens/feature/chatbot_feature.dart';
+import 'package:ai_realm/screens/feature/image_feature.dart';
+import 'package:ai_realm/screens/feature/language_feature.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
 enum homeType { aiChatbot, aiImgage, aiTranslator }
 
 extension myHomeType on homeType {
@@ -17,5 +24,17 @@ extension myHomeType on homeType {
         homeType.aiChatbot => true,
         homeType.aiImgage => false,
         homeType.aiTranslator => true,
+      };
+
+  EdgeInsets get padding => switch (this) {
+        homeType.aiChatbot => EdgeInsets.zero,
+        homeType.aiImgage => const EdgeInsets.all(20),
+        homeType.aiTranslator => EdgeInsets.zero,
+      };
+
+  VoidCallback get onTap => switch (this) {
+        homeType.aiChatbot => () => Get.to(() => ChatbotFeature()),
+        homeType.aiImgage => () => Get.to(() => ImageFeature()),
+        homeType.aiTranslator => () => Get.to(() => const LanguageFeature()),
       };
 }
