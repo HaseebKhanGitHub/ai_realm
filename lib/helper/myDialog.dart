24b28680921abe3c7f27fa3c242aa5a7569af3ae
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-12-22 15:28:36 +0500

    commit

---
+++ lib/helper/myDialog.dart
@@ -0,0 +1,26 @@
+import 'package:ai_realm/main.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+class myDialog {
+  //info
+  static void info(String msg) {
+    Get.snackbar("Info", msg,
+        backgroundColor: Colors.redAccent.shade200,
+        colorText: Colors.white,
+        duration: Duration(seconds: 2));
+  }
+
+//success
+  static void success(String msg) {
+    Get.snackbar("Success", msg,
+        backgroundColor: Colors.redAccent.shade200, colorText: Colors.white);
+  }
+
+//error
+  static void error(String msg) {
+    Get.snackbar("Error", msg,
+        backgroundColor: Colors.redAccent.shade200, colorText: Colors.white);
+  }
+}
