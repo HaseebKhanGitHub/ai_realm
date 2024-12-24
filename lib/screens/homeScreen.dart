f8738ea8d225232f1db61142afc7c0d42027b5ab
Abdul Haseeb Khan <128179003+HaseebKhanGitHub@users.noreply.github.com>
2024-12-24 23:15:49 +0500

    commit

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,8 +1,12 @@
+import 'package:ai_realm/api/appwrite.dart';
 import 'package:ai_realm/helper/global.dart';
 import 'package:ai_realm/model/home_type.dart';
+import 'package:ai_realm/screens/loginScreen.dart';
 import 'package:ai_realm/widgets/home_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class homeScreen extends StatefulWidget {
   const homeScreen({super.key});
@@ -12,6 +16,25 @@ class homeScreen extends StatefulWidget {
 }
 
 class _homeScreenState extends State<homeScreen> {
+  final appwriteService = appwrite();
+
+  Future<void> logout() async {
+    try {
+      // Attempt to delete the current session (if it exists)
+      await appwriteService.account.deleteSession(sessionId: 'current');
+      final user = await appwriteService.account.get();
+      print("User ID: ${user.$id}");
+
+      print("Email: ${user.email}");
+    } catch (e) {
+      // If no active session, this will fail silently or you can log the error
+      print('No active session or error occurred: $e');
+    }
+
+    // Navigate to the LoginScreen
+    Get.offAll(() => LoginScreen());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,9 +49,9 @@ class _homeScreenState extends State<homeScreen> {
         elevation: 2,
         actions: [
           IconButton(
-              onPressed: () {},
+              onPressed: logout,
               icon: Icon(
-                Icons.brightness_4_outlined,
+                Icons.logout,
                 color: Colors.blue,
                 size: 26,
               ))
@@ -36,7 +59,7 @@ class _homeScreenState extends State<homeScreen> {
       ),
       body: ListView(
         padding: EdgeInsets.symmetric(
-            horizontal: mq.width * .04, vertical: mq.height * .015),
+            horizontal: mq.size.width * .04, vertical: mq.size.height * .015),
         children: homeType.values
             .map((e) => home_card(
                   hometype: e,
